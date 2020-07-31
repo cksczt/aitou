ca476cefe226775eb05f708cda4a30bc40beefcd
cksczt <choukoushin@yahoo.co.jp>
2020-07-31 22:50:19 +0900

    NewGoods update

--- aitou-app/src/views/NewGood.vue
+++ aitou-app/src/views/NewGood.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="ImageUpload">
-    <p>
-      <router-link to="/">Home</router-link>
-    </p>
-    <h1>This is an newgood page</h1>
+  <div id="newgood">
+    <div id="imageupload" class="imageuploads">
+      <div id="gooditems" class="goodimage">item1</div>
+      <div id="gooditems" class="goodimage">item2</div>
+      <div id="gooditems" class="goodimage">item3</div>
+      <div id="gooditems" class="goodimage">item4</div>
+      <div id="gooditems" class="goodimage">item5</div>
+      <div id="gooditems" class="goodimage">item6</div>
+      <div id="gooditems" class="goodimage">item7</div>
+      <div id="gooditems" class="goodimage">item8</div>
+      <div id="gooditems" class="goodimage">item9</div>
+      <div id="gooditems" class="goodimage">item10</div>
+    </div>
   </div>
 </template>
-<style scoped>
-.ImageUpload {
-  width: 950px;
+<style >
+.imageuploads {
+  margin: 5px 10px 5px 30px;
+  color: #dddddd;
+  border: solid;
+  width: 330px;
+  height: 80px;
+  overflow: scroll;
+  display: inline-flex;
+}
+.goodimage {
+  margin: 5px 10px 5px 30px;
+  color: #dddddd;
+  border: dotted;
+  width: 100px;
+  height: 53px;
 }
 </style>
