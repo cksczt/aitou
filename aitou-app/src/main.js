e0e59c03cd6ee28824527ae3f671db28edaa1b20
cksczt <choukoushin@yahoo.co.jp>
2020-08-02 23:16:41 +0900

    main.js up

--- aitou-app/src/main.js
+++ aitou-app/src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Ionic from '@ionic/vue'
 import './registerServiceWorker'
 import './plugins/ionic.js'
-
 
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [/^ion-/]
@@ -12,5 +12,6 @@ Vue.config.ignoredElements = [/^ion-/]
 new Vue({
   router,
   store,
+  Ionic,
   render: h => h(App)
 }).$mount('#app')
