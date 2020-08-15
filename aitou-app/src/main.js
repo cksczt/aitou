82f9d9b0a3ffe1a128784dfae24ff5a6923ec5d3
cksczt <choukoushin@yahoo.co.jp>
2020-08-15 23:39:27 +0900

    Viewer

--- aitou-app/src/main.js
+++ aitou-app/src/main.js
@@ -7,17 +7,12 @@ import Ionic from '@ionic/vue'
 import './registerServiceWorker'
 import './plugins/ionic.js'
 
-Vue.use(Ionic)
-Vue.use(Viewer, { name: 'viewer' })
-Vue.directive('viewer', Viewer)
-Vue.config.productionTip = false
-Vue.config.ignoredElements = [/^ion-/]
-
+Vue.use(Viewer, Ionic)
 new Vue({
   el: '#app',
   router,
   store,
-  Ionic,
   Viewer,
+  Ionic,
   render: h => h(App)
 }).$mount('#app')
