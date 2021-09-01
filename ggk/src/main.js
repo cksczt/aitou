f019614fe35883336c26b75c5a3b3b1fb803cfbd
cksczt <choukoushin@yahoo.co.jp>
2021-09-01 11:15:07 +0800

    追加ionic

--- ggk/src/main.js
+++ ggk/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Ionic from '@ionic/vue'
+import '@ionic/core/css/ionic.bundle.css'
+import Vue from 'vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+Vue.use(Ionic);
